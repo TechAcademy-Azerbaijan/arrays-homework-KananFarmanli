7d8c0c3dbd58c32ccdce0020a6149aa6178f0ad8
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 17:23:51 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -1,9 +1,7 @@
-const prompt = require('prompt');
-prompt.start();
+let arr8 = [6, 0, -7, 2, -3, 5, 7]
+let copy8 = []
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for (let i = arr8.length-1; i>=0;i--) {
+  copy8.push(arr8[i])
+}
+console.log(copy8)
