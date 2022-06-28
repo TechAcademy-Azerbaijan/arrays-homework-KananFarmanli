ca80c2b871f03196c005bf8f0a3cc2e1137c29e2
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 16:54:09 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,43 @@
-const prompt = require('prompt');
-prompt.start();
+let arr5 = [6, 2, 7, 1, 7, 1,4,5,4,2, 2]; //10
+let copy5 = [...arr5];
 
+let max5 = 0;
+let min5 = arr5[0];
+let res5 = 0;
+for (let j = 0; j < arr5.length; j++) {
+  //res5 = 0;
+  for (let i = 0; i < copy5.length; i++) {
+    if (copy5[i] > max5) {
+      max5 = copy5[i];
+    }
 
-prompt.get('input', function (err, result) {
+    if (copy5[i] < min5) {
+      min5 = copy5[i];
+    }
+  }
+  //console.log("max /min", max5, min5);
+  let index1 = arr5.indexOf(max5);
+  let index2 = arr5.indexOf(min5);
+  // console.log('massiv',arr)
+  // console.log('index1',index1)
+  // console.log('index2',index2)
+
+  if (arr5.includes(max5)) {
+    arr5.splice(index1, 1);
+  }
+
+  if (arr5.includes(min5)) {
+    arr5.splice(index2, 1);
+  }
+
+  res5 = 0;
+  //console.log(arr5, "arr5555");
   
-    // Write code here
   
-});
+  for (let i = 0; i < arr5.length; i++) {
+   // console.log('resss',res5);
+    res5 = res5 + arr5[i];
+  }
+}
+console.log("arr", arr5);
+console.log(res5);
