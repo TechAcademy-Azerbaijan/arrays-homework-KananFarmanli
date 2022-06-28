85ae5ff1fc07aed14ef09c0f986b3e5980d53ce5
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 17:28:21 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,15 @@
-const prompt = require('prompt');
-prompt.start();
+let arr3 = [14, 16, 3,4, 7, 17, 19, 9,20,22,2,1,2] //16 7 17 19
+let index3 = []
+let max3= 0
+let count3 = 1
+
+for(let i = 0; i<arr3.length; i++) {
+
+    if (arr3[i]<arr3[i+1]) {
+        index3.push(arr3[i+1])
+           }
+ 
+}
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+console.log(index3)
