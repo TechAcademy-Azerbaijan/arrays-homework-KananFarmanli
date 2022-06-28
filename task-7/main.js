3bf9c23444f9709020401ef1211da98db6091ca6
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 17:11:25 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
+let arr6 = [6, -3, 5, -5, -4, 7, -5]  //-5 -3 5 6 -4 7 -5
 
+let min6 = arr6[0]
+let tmp 
+for (let i = 0; i<arr6.length; i++) {
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+  if(arr6[i]<min6) {
+    min6 = arr6[i]
+  }
+
+}
+let index6 = arr6.indexOf(min6)
+
+console.log(index6)
+
+tmp =arr6[0]
+arr6[0] = arr6[index6]
+arr6[index6]=tmp
+
+console.log(arr6)
