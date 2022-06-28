b9ed48305135ec5a39bd6cf87972df217be64be7
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 16:59:11 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,31 @@
-const prompt = require('prompt');
-prompt.start();
+let arr4 = [3, 5, -7, 7, 5, -9, -4]  //5 3 7 -7 -9 5 -4
+
+let res4 = []
+
+// for(let i = 0; i<arr4.length; i++){
+
+//     if (i==0) {
+//         res4.push(arr4[i+1])
+//         res4.push(arr4[i])
+//     }else if (i%2==0 && arr4[i+1]!=undefined) {
+//         res4.push(arr4[i+1])
+//         res4.push(arr4[i])
+//     } else if (i%2==0 && arr[i+1]==undefined){
+//         res4.push(arr4[i])
+//     }
+
+// }
+// console.log(res4)
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+for(let i = 1; i<arr4.length;i=i+2 ){
+    console.log('-------',i);
+        res4.push(arr4[i])
+        res4.push(arr4[i-1])
+    
+       if(i+1==arr4.length-1) {
+        res4.push(arr4[arr4.length -1])
+       }
+    
+    }
+    console.log(res4)
