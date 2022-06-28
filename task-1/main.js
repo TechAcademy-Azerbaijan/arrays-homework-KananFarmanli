a6fc2ccc16e37fc3aa0b6650e0c04fe80e109839
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-06-28 15:41:14 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -13,26 +13,18 @@ prompt.get('input', function (err, result) {
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
     let arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
     //let arr = [3, 5, -7, 7, 5, -9, -4];
-    let count = 0
-for (let i = 0; i < arr.length; i++) {
-    console.log(i)
-    for (let j = 0; j < arr.length; j++) {
-    if (i !== j) {
-        console.log('nawe iiiiiiii',i)
-        console.log('nawe jjjjjj',j)
-      if (arr[i] === arr[j]) {
-        console.log(arr)
-        arr.splice(j,1)
-        console.log(arr)
-        count++      
-        break;
-        
-      }
+    let res = []
+
+for(let i = 0; i<arr.length; i++) {
+    if (res.includes(arr[i])){
+
+    }else {
+        res.push(arr[i])
     }
-  }
-
-
-
 }
-console.log(arr.length); // 34
+
+
+console.log(res)
+
+console.log(res.length); // 34
 });
